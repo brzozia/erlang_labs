ffc285fef2aa82b73764edfabe678a893ad87cfb
Natalia <natib13@gmail.com>
2020-04-17 22:06:46 +0200

    pollution first part done

--- pollution.erl
+++ pollution.erl
@@ -10,7 +10,7 @@
 -author("Natalia Brzozowska").
 
 %% API
--export([createMonitor/0, addStation/3, reallyAddStation/3,addValue/5,removeValue/4,getOneValue/4]).
+-export([createMonitor/0, addStation/3, reallyAddStation/3,addValue/5,removeValue/4,getOneValue/4, getStationMean/3,getDailyMean/3,getTuples/5]).
 %%, getDailyMean/0, getOneValue/0, getStationMean/0]).
 
 %%-----------------------data structure---------------------------
@@ -84,13 +84,39 @@ getOneValue(Monitor, Id, Date, Type) ->
 
 %%------------getStationMean------------
 
-getStationMean()
+getStationMean(Monitor, Id, Type) ->
+  Name = getStationName(Monitor,Id),
+  {_,_, Measurements}= dict:fetch(Name, Monitor#monitor.stationsData),
+  TypeList = lists:filter(
+    fun
+      (Elem) when (Elem#measurement.type==Type) -> true;
+      (_)->false end, Measurements),
+  lists:foldl(fun(X, Sum) -> X#measurement.value + Sum end, 0, TypeList) / lists:foldl(fun(_, Sum) -> 1 + Sum end, 0, TypeList).
+
+%%------------getDailyMean------------
+
+getDailyMean(Monitor, Date,Type) ->
+  Stations = dict:fetch_keys(Monitor#monitor.stationsData),
+  TuplesList = getTuples(Monitor, Stations, Date, Type, []),
+  lists:foldl(fun(X, Sum) -> X#measurement.value + Sum end, 0, TuplesList) / lists:foldl(fun(_, Sum) -> 1 + Sum end, 0, TuplesList).
+
+getTuples(_, [], _, _, TuplesList) ->
+  TuplesList;
+getTuples(Monitor, [H | Stations], DateTime={DateV, _}, Type, TuplesList) ->
+  {_,_, Measurements}= dict:fetch(H, Monitor#monitor.stationsData),
+  NewTuplesList = TuplesList ++ lists:filter(
+      fun
+      (Elem) when (Elem#measurement.type==Type) -> {Date, _} = Elem#measurement.date, Date==DateV;
+      (_)->false end,Measurements),
+  getTuples(Monitor,Stations, DateTime, Type, NewTuplesList).
 
 
-%%getDailyMean()
 %%M = pollution:createMonitor().
 %%M2 = pollution:addStation(M, "Krakow", {31.23,45.67}).
-%%M3 = pollution:addValue(M2, {31.23,45.67}, "dd", "mjg", 23.4).
-%%M4 = pollution:addValue(M3, {31.23,45.67}, "daaad", "mjg", 23.4).
-%%M32 = pollution:removeValue(M4,{31.23,45.67}, "daaad", "mjg" ).
-%%pollution:getOneValue(M32,"Krakow", "dd", "mjg").
+%%M3 = pollution:addValue(M2, {31.23,45.67}, calendar:local_time(), "mjg", 23.4).
+%%M4 = pollution:addValue(M3, {31.23,45.67}, calendar:local_time(), "mjgt", 23.9).
+%%M32 = pollution:removeValue(M4,{31.23,45.67}, calendar:local_time(), "mjg" ).
+%%pollution:getOneValue(M32,"Krakow", calendar:local_time(), "mjgt").
+%%pollution:getStationMean(M32,"Krakow","mjgt").
+%%M7 = pollution:addValue(M32, "Krakow", calendar:local_time(), "mjg", 9).
+%%pollution:getStationMean(M7,"Krakow","mjg").
