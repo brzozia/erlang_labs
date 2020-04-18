aa37852116e639a7eb48db41b2854dabc992c6ce
Natalia <natib13@gmail.com>
2020-04-18 02:11:11 +0200

    almost end

--- pollution.erl
+++ pollution.erl
@@ -10,8 +10,8 @@
 -author("Natalia Brzozowska").
 
 %% API
--export([createMonitor/0, addStation/3, reallyAddStation/3,addValue/5,removeValue/4,getOneValue/4, getStationMean/3,getDailyMean/3,getTuples/5]).
-%%, getDailyMean/0, getOneValue/0, getStationMean/0]).
+-export([createMonitor/0, addStation/3, reallyAddStation/3,addValue/5,removeValue/4,getOneValue/4, getStationMean/3,getDailyMean/3,getTuples/5, existMeasure/3, getWorstDay/3,compareMeasurements/3]).
+
 
 %%-----------------------data structure---------------------------
 %%
@@ -27,11 +27,14 @@
 
 %%-----------------------functions---------------------------------
 %%-----------createMonitor----------
+%% 'createMonitor' is a function which creates monitor containing two dictionaries.
 
 createMonitor() -> #monitor{stations=dict:new(), stationsData=dict:new()}.
 
 
 %%------------addStation------------
+%% 'addStation' is a function which firstly checks whether station already exist in monitor, and if not
+%% calls 'reallyAddStation'  which adds station's data to both monitor's dictionaries, and returns new updated monitor.
 
 addStation(Monitor, Name, Coordinates)  ->
   case ( dict:is_key(Name,  Monitor#monitor.stationsData) or dict:is_key(Coordinates,  Monitor#monitor.stations)) of
@@ -46,23 +49,47 @@ reallyAddStation(Monitor, Name, Coordinates) ->
 
 
 %%------------addValue------------
+%% 'addValue' is a function, which adds value to monitor to given dictionary key, after checking whether there already is such measurement in monitor's dictionary. It adds Measure to measurements' list
+%% It calls functions 'existMeasure' and 'getStationName'.
 
 addValue(Monitor, Id, Date, Type, Value ) ->
   Measure = #measurement{type=Type, value =Value, date=Date},
   Name = getStationName(Monitor,Id),
-  #monitor{stations= Monitor#monitor.stations,
-    stationsData=dict:update(Name,
-    fun(Old) -> #stationData{coordinates = Id, measurements = Old#stationData.measurements ++[Measure]} end,
-    Monitor#monitor.stationsData)}.
 
-getStationName(Monitor, Id) ->
-  case is_tuple(Id) of
-    true -> {_,Name} = dict:find(Id, Monitor#monitor.stations);
-    false -> Name=Id
-  end,
+  case existMeasure(Monitor,Name,Measure) of
+    true -> io:put_chars(standard_error, "There already exists a measurement with this features\n");
+    false -> #monitor{stations= Monitor#monitor.stations,
+                      stationsData=dict:update(Name,
+                        fun(Old) -> #stationData{coordinates = Id, measurements = Old#stationData.measurements ++[Measure]} end,
+                        Monitor#monitor.stationsData)}
+  end.
+
+%% Function 'existMeasure' check whether the measure, which we want to add already exist in monitor.
+
+existMeasure(Monitor,Name,Measure) ->
+  {_,_, Measurements}= dict:fetch(Name, Monitor#monitor.stationsData),
+  List = lists:filter(
+    fun
+      (Elem) when Elem==Measure -> true;
+      (_)->false end, Measurements),
+  case List of
+    [] -> false;
+    _ -> true
+  end.
+
+%% Function 'getStationName' returns name of station basing on given Id. Id can be a station's coordinates or name.
+
+getStationName(Monitor, Id) when is_tuple(Id) ->
+  case dict:is_key(Id,Monitor#monitor.stations) of
+    true -> {_,Name} = dict:find(Id, Monitor#monitor.stations), Name; %% if Station does not exist in dict, dict:find returns error
+    false ->  io:put_chars(standard_error, "There is no such Station")
+  end;
+getStationName(_, Id) ->
+  Name=Id,
   Name.
 
 %%------------removeValue------------
+%% This function removes a value connected to given station's Id and specified Date and Type of measurement. It updates the dictionary, removing value from measurements' list.
 
 removeValue(Monitor, Id, Date, Type) ->
   Name = getStationName(Monitor,Id),
@@ -72,6 +99,7 @@ removeValue(Monitor, Id, Date, Type) ->
       Monitor#monitor.stationsData)}.
 
 %%------------getOneValue------------
+%% Returns value from specified measurement of specified station, basing on station's Id. It gets list of measurements of the station and then filters this list.
 
 getOneValue(Monitor, Id, Date, Type) ->
   Name = getStationName(Monitor,Id),
@@ -83,6 +111,7 @@ getOneValue(Monitor, Id, Date, Type) ->
   Val.
 
 %%------------getStationMean------------
+%% Function 'getStationMean' returns mean of all measurements of specified type of given station. Firstly it gets list of measurements of the station and then filters this list. After that it uses foldl to compute the mean.
 
 getStationMean(Monitor, Id, Type) ->
   Name = getStationName(Monitor,Id),
@@ -93,23 +122,57 @@ getStationMean(Monitor, Id, Type) ->
       (_)->false end, Measurements),
   lists:foldl(fun(X, Sum) -> X#measurement.value + Sum end, 0, TypeList) / lists:foldl(fun(_, Sum) -> 1 + Sum end, 0, TypeList).
 
-%%------------getDailyMean------------
 
-getDailyMean(Monitor, Date,Type) ->
+%%------------getDailyMean------------
+%% Day should be a date() tuple - {year,month,day}
+%% Function returns a mean of all measurements of specified type and day from all stations. Firstly it gets all stations' names and then, using 'getTuples' all tuples containing that day's measurements. After computes mean using foldl.
+
+getDailyMean(Monitor, Day,Type) ->
   Stations = dict:fetch_keys(Monitor#monitor.stationsData),
-  TuplesList = getTuples(Monitor, Stations, Date, Type, []),
+  TuplesList = getTuples(Monitor, Stations, Day, Type, []),
   lists:foldl(fun(X, Sum) -> X#measurement.value + Sum end, 0, TuplesList) / lists:foldl(fun(_, Sum) -> 1 + Sum end, 0, TuplesList).
+
+
+%% 'getTuples' returns a list of tuples, which contains measurements from given day. Function search whole monitor dictionary to find that tuples and adds them to final list.
 
 getTuples(_, [], _, _, TuplesList) ->
   TuplesList;
-getTuples(Monitor, [H | Stations], DateTime={DateV, _}, Type, TuplesList) ->
+getTuples(Monitor, [H | Stations], DateV, Type, TuplesList) ->
   {_,_, Measurements}= dict:fetch(H, Monitor#monitor.stationsData),
   NewTuplesList = TuplesList ++ lists:filter(
       fun
       (Elem) when (Elem#measurement.type==Type) -> {Date, _} = Elem#measurement.date, Date==DateV;
       (_)->false end,Measurements),
-  getTuples(Monitor,Stations, DateTime, Type, NewTuplesList).
+  getTuples(Monitor,Stations, DateV, Type, NewTuplesList).
 
+
+%%------------getWorstDay------------
+%% Function 'getWorstDay' returns the worst daytime and value of all measurements from given type and given station. It makes list of measurements of given type and from given station and calls 'compareMeasurements' with this list.
+
+getWorstDay(Monitor, Id, Type) ->
+  Name = getStationName(Monitor,Id),
+  {_,_, Measurements}= dict:fetch(Name, Monitor#monitor.stationsData),
+  TypeMeasurements = lists:filter(
+    fun
+      (Elem) when (Elem#measurement.type==Type) -> true;
+      (_)->false end,Measurements),
+  compareMeasurements(TypeMeasurements,{}, 0 ).
+
+
+%% 'compareMeasurements' compares measurements in given list. It returns datetime of the measurement with the highest value and this value.
+
+compareMeasurements([],WorstDate, WorstVal) ->
+  {WorstDate, WorstVal};
+compareMeasurements([H | Tail],WorstDate, WorstVal) ->
+  case H#measurement.value > WorstVal of
+    true -> compareMeasurements(Tail,H#measurement.date, H#measurement.value);
+    false -> compareMeasurements(Tail,WorstDate, WorstVal)
+  end.
+
+
+
+
+%%test:
 
 %%M = pollution:createMonitor().
 %%M2 = pollution:addStation(M, "Krakow", {31.23,45.67}).
@@ -120,3 +183,6 @@ getTuples(Monitor, [H | Stations], DateTime={DateV, _}, Type, TuplesList) ->
 %%pollution:getStationMean(M32,"Krakow","mjgt").
 %%M7 = pollution:addValue(M32, "Krakow", calendar:local_time(), "mjg", 9).
 %%pollution:getStationMean(M7,"Krakow","mjg").
+%%M8 = pollution:addValue(M7, {31.23,45.67}, calendar:local_time(), "mjg", 23.4).
+%%M9 = pollution:addValue(M8, {31.23,45.67}, calendar:local_time(), "mjg", 23.4).
+%%pollution:getWorstDay(M8,"Krakow", "mjg").
